da4d8a4bf013957bd7ba9f0eb7c8b377867df14a
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2022-02-24 12:00:33 +0900

    변수와 데이터 유형 연습 문제

---
+++ Haeun/3. 코틀린 기초/변수와 데이터 유형 연습문제.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    var myClass: String = "Android Masterclass"
+   	val myFloat: Float = 13.37F
+    val pi: Double = 3.14159265358979
+    var myInt: Int = 25
+    var year: Int = 2022
+    var myNum: Long = 18_881_234_567
+    var myBool: Boolean = true
+    var myAlphabet: Char = 'a'
+    
+    println("$myClass  $myFloat  $pi  $myInt  $year")
+    println("$myNum  $myBool  $myAlphabet")
+    
+}
